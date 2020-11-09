15a8581a52dce2da0b753a6fc0669678c88ff3a5
benk1 <kakengi_bernard@hotmail.com>
2020-11-09 16:31:08 +0200

    finish calling our back-end implementation

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,29 +3,29 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoint = apiUrl + '/movies';
 
-/*function movieUrl(id) {
+function movieUrl(id) {
   return `${apiEndpoint}/${id}`;
-}*/
+}
 
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + '/' + movieId); //(movieUrl(movieId));
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndpoint + '/' + movie._id, body); //movieUrl(movie._id), body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  //return http.delete(movieUrl(movieId));
-  return http.get(apiEndpoint + '/' + movieId);
+  return http.delete(movieUrl(movieId));
+  //return http.get(apiEndpoint + '/' + movieId);
 }
