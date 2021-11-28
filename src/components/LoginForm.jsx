bb0eb165f5192edbffb828b4292fe0f2ccec8f99
ben <kakengi_bernard@hotmail.com>
2021-11-28 09:20:36 +0200

    rectify logout Link in Navbar

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,52 +2,50 @@ import React, { Component } from 'react';
 import Joi from 'joi-browser';
 //import Input from './common/Input';
 import Form from './common/Form';
-import auth from '../services/authService'
-import {Redirect} from 'react-router-dom'
+import auth from '../services/authService';
+import { Redirect } from 'react-router-dom';
 
 class LoginForm extends Form {
-  state = {
-    data: { username: '', password: '' },
-    errors: {},
-  };
+	state = {
+		data: { username: '', password: '' },
+		errors: {},
+	};
 
-  schema = {
-    username: Joi.string().required().label('Username'),
-    password: Joi.string().required().label('Password'),
-  };
+	schema = {
+		username: Joi.string().required().label('Username'),
+		password: Joi.string().required().label('Password'),
+	};
 
-  doSubmit = async() => {
-    //console.log('Submitted');
-  try {
-    
-    const{data} = this.state
-     await auth.login(data.username,data.password)
+	doSubmit = async () => {
+		//console.log('Submitted');
+		try {
+			const { data } = this.state;
+			await auth.login(data.username, data.password);
 
-     const {state} = this.props.location
-    window.location = state ? state.from.pathname : '/'
-  } catch (ex) {
-    if(ex.response && ex.response.status === 400){
-      const errors = {...this.state.errors} // clone the errors object from the state
-      errors.username = ex.response.data //this will display error from the server
-      this.setState({errors}) // set the errors ES6--keys == values
-    }
-    
-  }
-  };
-  render() {
-    if(auth.getCurrentUser()) return <Redirect to='/' />
-    return (
-      <div className='row'>
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit} className='col s12'>
-          {this.renderInput('username', 'Username')}
-          {this.renderInput('password', 'Password', 'password')}
+			const { state } = this.props.location;
+			window.location = state ? state.from.pathname : '/';
+		} catch (ex) {
+			if (ex.response && ex.response.status === 400) {
+				const errors = { ...this.state.errors }; // clone the errors object from the state
+				errors.username = ex.response.data; //this will display error from the server
+				this.setState({ errors }); // set the errors ES6--keys == values
+			}
+		}
+	};
+	render() {
+		if (auth.getCurrentUser()) return <Redirect to="/" />;
+		return (
+			<div className="row">
+				<h1>Login</h1>
+				<form onSubmit={this.handleSubmit} className="col s12">
+					{this.renderInput('username', 'Username')}
+					{this.renderInput('password', 'Password', 'password')}
 
-          {this.renderButton('Login')}
-        </form>
-      </div>
-    );
-  }
+					{this.renderButton('Login')}
+				</form>
+			</div>
+		);
+	}
 }
 
 export default LoginForm;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,53 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-const NavBar = ({user}) => {
-  return (
-    <nav>
-      <div className='nav-wrapper grey darken-3    '>
-        <div className="container">
-
-        <Link to='/' className='brand-logo '>
-          Movie Rental
-        </Link>
-        <ul id='nav-mobile' className='left hide-on-med-and-down right '>
-          <li>
-            <NavLink to='/movies'>Movies</NavLink>
-          </li>
-          <li>
-            <NavLink to='/customers'>Customers</NavLink>
-          </li>
-          <li>
-            <NavLink to='/rentals'>Rentals</NavLink>
-          </li>
-          { !user && (
-          <>
-          <li>
-            <NavLink to='/login'>Login</NavLink>
-          </li>
-          <li>
-            <NavLink to='/register'>Register</NavLink>
-          </li>
-          </>
-          )}
-          <li>
-            <NavLink to='/logout'>Logout</NavLink>
-          </li>
-          { user && 
-          <>
-          <li>
-            <NavLink 
-            to='/profile' 
-            className="btn btn-floating pink lighten-3">{user.name[0]}</NavLink>
-            
-          </li>
-          </>
-          }
-        </ul>
-        </div>
-      </div>
-    </nav>
-  );
+const NavBar = ({ user }) => {
+	return (
+		<nav>
+			<div className="nav-wrapper grey darken-3    ">
+				<div className="container">
+					<Link to="/" className="brand-logo ">
+						Movie Rental
+					</Link>
+					<ul id="nav-mobile" className="left hide-on-med-and-down right ">
+						<li>
+							<NavLink to="/movies">Movies</NavLink>
+						</li>
+						<li>
+							<NavLink to="/customers">Customers</NavLink>
+						</li>
+						<li>
+							<NavLink to="/rentals">Rentals</NavLink>
+						</li>
+						{!user && (
+							<>
+								<li>
+									<NavLink to="/login">Login</NavLink>
+								</li>
+								<li>
+									<NavLink to="/register">Register</NavLink>
+								</li>
+							</>
+						)}
+						{user && (
+							<>
+								<li>
+									<NavLink to="/logout">Logout</NavLink>
+								</li>
+								<li>
+									<NavLink
+										to="/profile"
+										className="btn btn-floating pink lighten-3"
+									>
+										{user.name[0]}
+									</NavLink>
+								</li>
+							</>
+						)}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
